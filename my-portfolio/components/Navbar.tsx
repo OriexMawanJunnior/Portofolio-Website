0457e23229a32e676637b6e8bcf5a774772dbbc6
OriexMawanJunnior <oriex21001@mail.unpad.ac.id>
2023-08-24 17:25:26 +0700

    docs: deleteing eror

--- my-portfolio/components/Navbar.tsx
+++ my-portfolio/components/Navbar.tsx
@@ -2,9 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Logo from './Logo'
-import LinkedinIcon from './Icons'
-import GithubIcon from './Icons2'
-import {motion} from 'framer-motion'
+
 
 const Navbar = () => {
   return (
@@ -13,13 +11,6 @@ const Navbar = () => {
           <Link href='/'  className='mx-4'>Home</Link>
           <Link href='/about' className='mx-4'>About</Link>
           <Link href='/project' className='mx-4'>Project</Link>
-        </nav>
-        
-        <nav className='flex items-center justify-center flex-wrap'>
-          <motion.a href='/' target={"_blank"} 
-          whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mr-3'><LinkedinIcon></LinkedinIcon></motion.a>
-          <motion.a href='/' target={"_blank"}
-          whileHover={{y:-2}} whileTap={{scale:0.9}} className='w-6 mr-3'><GithubIcon></GithubIcon></motion.a>
         </nav>
 
         <div className="absolute left-[50%] top-2 translate-x-[-50%]">
